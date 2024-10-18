b6b4a3d918df2085f62b84dc17eacd3572502d5b
badgersky <szymonek.ptasznik@gmail.com>
2024-10-18 22:54:54 +0200

    database models

---
+++ models.py
@@ -0,0 +1,26 @@
+from typing import List
+from typing import Optional
+from sqlalchemy import ForeignKey
+from sqlalchemy import String
+from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.orm import Mapped
+from sqlalchemy.orm import mapped_column
+from sqlalchemy.orm import relationship
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+class Books(Base):
+    __tablename__ = "books"
+    
+    id: Mapped[int] = mapped_column(primary_key=True)
+    title: Mapped[str] = mapped_column(string(255)
+
+
+class PageCount(Base):
+    __tablename__ = "page_count"
+    
+    id: Mappend[int] = mapped_column(primary_key=True)
+    book_id: Mapped[int] = mapped_column(ForeignKey("books.id"))
