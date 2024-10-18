71e175473833bcfc538a163a73413e5694d0e5fb
badgersky <szymonek.ptasznik@gmail.com>
2024-10-19 00:16:53 +0200

    changing page num functionality

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from sqlalchemy import select
 from sqlalchemy.orm import Session
+from sqlalchemy import update
 
 from models import Book, PageCount
 from engine import get_engine
@@ -8,22 +9,33 @@ from engine import get_engine
 engine = get_engine()
 session = Session(engine)
 
-books = session.scalars(select(Book))
-pages = session.scalars(select(PageCount))
-
-print("Choose book")
-ids_pages = {}
-
-for book in books:
-    for page in pages:
-        if book.id == page.book_id:
-            print(book.id, book.title)
-            ids_pages[book.id] = page.page
-            
-ids = list(ids_pages.keys())
 while True:
-    choice = int(input(f"Choose book ({min(ids)}-{max(ids)}): "))
-    if choice in ids:
-        print(ids_pages[choice])
+    ans = input("Do you want to update page count (y/n)? ")
+    if ans in "yesno":
+        break
+
+if ans in "yes":
+    new_count = int(input("New page count: "))
+    query = select(PageCount).where(PageCount.book_id == 1)
+    page = session.scalars(query).one()
+    page.page = new_count
+    session.commit()
+elif ans in "no":
+    books = session.scalars(select(Book))
+    pages = session.scalars(select(PageCount))
+    ids_pages = {}
+
+    for book in books:
+        for page in pages:
+            if book.id == page.book_id:
+                print(book.id, book.title)
+                ids_pages[book.id] = page.page
+                
+    ids = list(ids_pages.keys())
+    while True:
+        choice = int(input(f"\nChoose book ({min(ids)}-{max(ids)}): "))
+        if choice in ids:
+            print(ids_pages[choice])
+            break
     
                    
